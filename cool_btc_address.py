5c5b0221644adc0f8c58847500cab90e3c000012
Kevin Haritmonds <haritmonds@yahoo.com>
2019-07-12 21:07:25 +0530

    Generate BTC addresses which has predefined prefix

---
+++ cool_btc_address.py
@@ -0,0 +1,10 @@
+import secrets
+
+from btc import BTC
+
+while True:
+    btc_private_key = secrets.token_bytes(nbytes=32)
+    btc_address = BTC.compute_btc_address(btc_private_key)
+    if btc_address.lower().startswith('1kev'):
+        btc_private_key_in_wif = BTC.convert_btc_private_key_into_wif(btc_private_key)
+        print('{} - {}'.format(btc_address, btc_private_key_in_wif))
